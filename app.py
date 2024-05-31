aab316994ef9b3753182bac91bf40d92ca6e4461
yannick <yannick93@live.nl>
2024-05-31 15:28:08 +0200

    Modified app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os  # Added for file saving
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 app = Flask(__name__)
 CORS(app)  # Apply CORS to all routes by default
 
 @app.route('/')
 def index():
-    return jsonify({"data": "success2"})
+    return jsonify({"data": "success3"})
 
 @app.route('/process-email-content', methods=['GET', 'POST'])
 def index2():
@@ -52,4 +54,9 @@ def index3():
         return jsonify({"data": "GET method not supported."})
 
 if __name__ == '__main__':
-    app.run(host='192.168.178.234', port=8001, debug=True)
+    app.run(host='192.168.178.234', port=8000, debug=True)
+    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+    port = int(os.getenv('FLASK_RUN_PORT', 5000))
+    debug = os.getenv('FLASK_DEBUG', 'false').lower() in ['true', '1', 't']
+
+    app.run(host=host, port=port, debug=debug)
